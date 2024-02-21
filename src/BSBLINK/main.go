3d0c28ad628d61feeba3b4c62b90feda84f85758
Sholahul Fajri <sholahulfajri30@gmail.com>
2024-02-21 18:30:55 +0700

    tgl 21022024

--- src/BSBLINK/main.go
+++ src/BSBLINK/main.go
@@ -1,20 +1,70 @@
 package main
 
 import (
+	"bsblink/controller"
+	"bsblink/model"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	fmt.Println("hello world")
-	router := mux.NewRouter()
-	router.HandleFunc("/login", login).Methods("POST")
-	http.ListenAndServe(":9090", router)
+type LoginResponse struct {
+	Name string `json:"name"`
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
-	log.Println("ada yang akses jam")
+type Body struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func main() {
+	err := godotenv.Load(".env") //
+	if err != nil {
+		log.Println(err, ".env not load")
+	}
+
+	db := model.ConnectDB()
+	defer db.Close()
+
+	//create router
+	router := mux.NewRouter()
+	router.HandleFunc("/Auth", Auth).Methods("POST")
+	router.HandleFunc("/GetUser", controller.GetUsers(db)).Methods("GET")
+	router.HandleFunc("/GetUserByID/{id}", controller.GetUserByID(db)).Methods("GET")
+
+	http.ListenAndServe(":9090", router)
+
+}
+
+func Auth(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	// Mengonversi waktu ke string dengan format default
+	timeString := now.String()
+	log.Println("Ada yang hit Jam : ", timeString)
+
+	// Read the content of the io.ReadCloser into a byte slice
+	content, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Convert the byte slice to a string
+	contentString := string(content)
+
+	// Print or use the string as needed
+	log.Println(contentString)
+
+	//CHECK USERNAME in body api
+	var b Body
+	err1 := json.Unmarshal([]byte(content), &b)
+	if err1 != nil {
+		fmt.Println("Error unmarshaling JSON:", err1)
+		return
+	}
 }
---
+++ src/BSBLINK/model/struct.go
@@ -0,0 +1,15 @@
+package model
+
+type User struct {
+	Id       int    `json:"id,omitempty"` //omitempty jika kosong tidak diberi balikan
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Role     string `json:"role,omitempty"`
+}
+
+type DataResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
