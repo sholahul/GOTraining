83a2e4ccdb928039b234cbef048ddcf8f0a94121
Sholahul Fajri <sholahulfajri30@gmail.com>
2024-03-01 21:32:11 +0700

    01032024

--- src/EchoApi/controller/Date.go
+++ src/EchoApi/controller/Date.go
@@ -7,14 +7,14 @@ import (
 
 func Get_date() string {
 	// Set the time zone to "Asia/Jakarta"
-	indonesiaTimeZone, err := time.LoadLocation("Asia/Jakarta")
+	TimeZone, err := time.LoadLocation("Asia/Jakarta")
 	if err != nil {
 		// Handle error if timezone loading fails
 		return fmt.Sprintf("Error loading timezone: %s", err)
 	}
 
 	// Get the current time in the Indonesian timezone
-	currentTime := time.Now().In(indonesiaTimeZone)
+	currentTime := time.Now().In(TimeZone)
 
 	// Format the time as per your requirement (e.g., RFC1123)
 	formattedTime := currentTime.Format(time.RFC1123)
